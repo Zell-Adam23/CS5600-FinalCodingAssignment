5b6907fc9e3f5d1526bc7f988a9e63009fb8fdcd
Zell-Adam23 <adamzell1223@gmail.com>
2025-12-05 11:51:31 -0600

    merged the files for part 1

--- part1/server.py
+++ part1/server.py
@@ -3,6 +3,18 @@ import socket
 HOST = "0.0.0.0"
 PORT = 5000
 
+output_file = "recieved.txt"
+updated_file = "updated.txt"
+
+def recv_all(connection, size):
+    data = b""
+    while len(data) < size:
+        chunk = connection.recv(4096)
+        if not chunk:
+            break
+        data += chunk
+    return data
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as x:
     x.bind((HOST,PORT))
     x.listen(1)
@@ -26,5 +38,30 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as x:
             if message.startswith("Bye"):
                 connection.sendall(b"Bye from Server-AdamZ\n")
                 break
+
+            if message.startswith("file"):
+                filesize = int(connection.recv(1024).decode())
+                connection.sendall(b"sizeok")
+
+                print(f"[SERVER] Expecting {filesize} bytes")
+
+                filedata = recv_all(connection, filesize)
+
+                print("file recieved")
+                print(filedata.decode(errors="ignore"))
+                print("\nEOF\n")
+
+                with open(output_file, "wb") as y:
+                    y.write(filedata)
+                
+                updated_data = filedata + b"\nThis is an added line from the server.\n"
+                with open(updated_file, "wb") as y:
+                    y.write(updated_data)
+
+                connection.sendall(str(len(updated_data)).encode())
+                connection.recv(1024)
+                connection.sendall(updated_data)
+
+                print("[SERVER] updated file sent back")
 
             connection.sendall(f"Server recieved: {message}".encode())
