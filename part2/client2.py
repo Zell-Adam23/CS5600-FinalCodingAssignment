2817c065e199542c10451bdc99ef76ea0e2afd2f
Zell-Adam23 <adamzell1223@gmail.com>
2025-12-05 14:51:22 -0600

    part 2 should be done

--- part2/client2.py
+++ part2/client2.py
@@ -1,11 +1,22 @@
 import socket
+import threading
 
 #ports can be adjusted later, this is just for testing purposes
 HOST = "127.0.0.1"
 PORT = 5000
 
+def message_listen(connection):
+    while True:
+        data = connection.recv(1024)
+        if not data:
+            break
+        print("\n[CHAT]", data.decode(), "\nYou: ", end="")
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as x:
     x.connect((HOST,PORT))
+    print("connected to chat server!")
+
+    threading.Thread(target=message_listen, args=(x, ), daemon=True).start()
 
     while True:
         message = input("You: ")
--- part2/server2.py
+++ part2/server2.py
@@ -1,29 +1,45 @@
 import socket
+import threading
 
 #ports can be adjusted later, this is just for testing purposes
 HOST = "0.0.0.0"
 PORT = 5000
 
+clients = []
+
+def broadcast_to_all(message, sender):
+    for x in clients:
+        if x!= sender:
+            try:
+                x.sendall(message.encode())
+            except:
+                x.close()
+                clients.remove(x)
+
+def handle_new_client(connection, address):
+    print(f"[SERVER] New Client connected: {address}")
+    with connection:
+        while True:
+            data =connection.recv(1024)
+            if not data:
+                break
+            message = data.decode().strip()
+            print(f"[{address}]: {message}")
+
+            if message == "exit":
+                break
+
+            broadcast_to_all(f"[{address}]: {message}", connection)
+
+    print(f"[SERVER] Client {address} disconnected")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as x:
     x.bind((HOST,PORT))
-    x.listen(1)
+    x.listen()
+    print("[SERVER] Server now up and running")
     
     while True:
         print(f"[SERVER] Listening on {HOST}:{PORT} for next client")
         connection, address = x.accept()
-
-        with connection:
-            print(f"[SERVER] Connected by {address}")
-            connection.sendall(b"Hello from Server-AdamZ\n")
-
-            while True:
-                data = connection.recv(1024)
-                if not data:
-                    break
-                message = data.decode().strip()
-                print("[CLIENT]:", message)
-
-                if message == "exit":
-                    print("[SERVER] Client disconnect")
-                    break
+        clients.append(connection)
+        threading.Thread(target=handle_new_client, args=(connection,address), daemon=True).start()
